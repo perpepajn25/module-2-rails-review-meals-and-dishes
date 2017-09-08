bbd6a674d10ebab708afbbbfc407a2a86ceea745
Natalie Perpepaj <perpepajn25@gmail.com>
2017-09-08 14:25:50 -0400

    edit

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -3,5 +3,28 @@ class MealsController < ApplicationController
   def index
     @meals = Meal.all
   end
-  
+
+  def show
+      @meal = Meal.find(params[:id])
+  end
+
+  def edit
+    @meal = Meal.find(params[:id])
+  end
+
+  def update
+    @meal = Meal.find(params[:id])
+    if @meal.update(meal_params)
+      redirect_to meal_path(@meal)
+    else
+      render edit_meal_path(@meal)
+    end
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name,:location,:date)
+  end
+
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,5 +2,8 @@ class Meal < ApplicationRecord
   has_many :meal_dishes
   has_many :dishes, through: :meal_dishes
 
-  
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+
 end
